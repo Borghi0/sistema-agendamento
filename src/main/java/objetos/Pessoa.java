47cd1d08127cb3d0193ad989684cafa56bde470d
Borghi0 <136107203+Borghi0@users.noreply.github.com>
2025-05-05 21:36:28 -0300

    Create Pessoa.java

---
+++ src/main/java/objetos/Pessoa.java
@@ -0,0 +1,16 @@
+// src/main/java/objetos/Pessoa.java
+import java.util.List;
+
+public class Pessoa{
+  // atributos
+  private String nome;
+  
+  private List<Palestra> palestras
+
+  
+
+  
+  // getters e setter
+
+  
+}
