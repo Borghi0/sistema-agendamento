745610db5ec8212cf7ca4a65a5d9ed08911c9a68
Borghi0 <136107203+Borghi0@users.noreply.github.com>
2025-05-05 21:04:28 -0300

    Update Palestra.java

--- src/main/java/objetos/Palestra.java
+++ src/main/java/objetos/Palestra.java
@@ -1,9 +1,17 @@
 // src/main/java/objetos/Palestra.java
+import java.time.LocalTime;
+import java.time.LocalDate;
 
-Public class Palestra{
+public class Palestra{
   // atributos
+  private String titulo;
+  private String local; // criar uma classe endereço se possível
+  private LocalDate data;
+  private LocalTime hora;
 
-  
+  private List<Pessoa> palestrante; // criar classe pessoa
+  private List<Pessoa> participante; // criar classe pessoa
+
   // getters e setter
 
   
