01402321b0f051abf2fafa06132bb5d2e40540e0
joaop <joaop@10.0.22.5>
2025-05-07 23:37:59 -0300

    primeiraB

---
+++ src/main/java/objetos/Usuario.java
@@ -0,0 +1,12 @@
+// src/main/java/objetos/Usuario.java
+package main.java.objetos;
+
+public class Usuario extends Pessoa{
+    // atributos
+    private String email;
+    private String senha;
+    private boolean adm_flag;
+    
+    
+    // getters e setters
+}
