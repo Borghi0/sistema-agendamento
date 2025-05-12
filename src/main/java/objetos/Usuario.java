a76d20cec7bafc17dbdeba07ab89b83a569caf10
joaop <joaop@10.0.22.5>
2025-05-12 20:16:29 -0300

    colab

--- src/main/java/objetos/Usuario.java
+++ src/main/java/objetos/Usuario.java
@@ -6,7 +6,8 @@ public class Usuario extends Pessoa{
     private String email;
     private String senha;
     private String user;
-    private boolean adm_flag;
+    private boolean adm_flag;     // flag de admin
+    private boolean colab_flag;  // flag de colaborador
     
     
     //construtor
@@ -15,12 +16,14 @@ public class Usuario extends Pessoa{
         this.senha = "";
         this.user = "";
         this.adm_flag = false;
+        this.colab_flag = false;
     }
     public Usuario(String email, String senha, String user, boolean adm_flag){
         this.email = email;
         this.senha = senha;
         this.user = user;
         this.adm_flag = adm_flag;
+        this.colab_flag = colab_flag;
     }
 
     //getters
@@ -39,6 +42,9 @@ public class Usuario extends Pessoa{
     public boolean getAdm_flag(){
         return adm_flag;
     }
+    public boolean getColab_flag(){
+        return colab_flag;
+    }
 
     //setters
     public void setEmail(String email){
@@ -55,5 +61,8 @@ public class Usuario extends Pessoa{
 
     public void setAdm_flag(boolean adm_flag){
         this.adm_flag = adm_flag;
+    }
+    public void setColab_flag(boolean colab_flag){
+        this.colab_flag = colab_flag;
     }
 }
