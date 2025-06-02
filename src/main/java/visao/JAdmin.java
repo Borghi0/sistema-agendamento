a23f908903393ac5478729c21d47cefea2e1869a
joaop <joaop@10.0.22.5>
2025-06-02 20:08:32 -0300

    interface grafica

--- src/main/java/visao/JAdmin.java
+++ src/main/java/visao/JAdmin.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+// src/main/java/visao/JAdmin.java
 package main.java.visao;
 
 /**
